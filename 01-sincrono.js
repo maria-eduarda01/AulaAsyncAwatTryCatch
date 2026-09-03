d62ec82393e6ed65e16fcf33c2159505d4f4a750
MARIA EDUARDA BISCUOLA MARCHI <26172476@edu119.local>
2026-09-03 11:53:12 -0300

    Salvando Projeto

---
+++ 01-sincrono.js
@@ -0,0 +1,5 @@
+// O javaScript executa uma instrução depois da outra
+
+console.log("Inicio");
+console.log("Executando");
+console.log("Fim");
---
+++ 02-setTimeout.js
@@ -0,0 +1,7 @@
+console.log("Pedido realizado");
+
+setTimeout(() => {
+    console.log("Pedido ficou pronto");
+},3000);
+
+console.log("3 - Cliente pode continuar aguardando");
